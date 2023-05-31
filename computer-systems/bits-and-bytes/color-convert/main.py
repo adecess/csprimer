1ade522ddf986aa686951b67b503909f65f002ec
Axel Saliniere <axel.saliniere@protonmail.com>
2023-05-31 21:57:44 +0200

    tech(color-convert): simple.css

--- computer-systems/bits-and-bytes/color-convert/main.py
+++ computer-systems/bits-and-bytes/color-convert/main.py
@@ -2,8 +2,17 @@ import re
 import sys
 
 
+def hex_to_int(hex_str):
+    hex_numbers = "0123456789abcdef"
+    integer = 0
+    for i in hex_str:
+        integer = integer * 16 + hex_numbers.index(i)
+    return integer
+
+
 def hex_to_rgb(match_obj):
-    return match_obj.group()[1:]
+    hex_color = match_obj.group()[1:]
+    return "rgb(" + " ".join([str(hex_to_int(hex_color[i:i+2])) for i in range(0, len(hex_color), 2)]) + ")"
 
 
 if __name__ == "__main__":
