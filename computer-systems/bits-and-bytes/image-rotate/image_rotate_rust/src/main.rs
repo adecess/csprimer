08845d7e5314fdbba4c4014a9998c582a956bef2
adecess <axel.saliniere@protonmail.com>
2025-10-30 00:34:58 +0100

    image-rotate: parse the first pixel

--- computer-systems/bits-and-bytes/image-rotate/image_rotate_rust/src/main.rs
+++ computer-systems/bits-and-bytes/image-rotate/image_rotate_rust/src/main.rs
@@ -1,13 +1,25 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Error, Read};
 
 fn main() -> std::io::Result<()> {
     let mut f = File::open("teapot.bmp")?;
-    let mut buffer = [0u8; 2];
 
-    // read 2 bytes
-    f.read_exact(&mut buffer)?;
+    let mut buffer = Vec::new();
 
-    println!("The bytes: {:#02x?}", &buffer);
+    // read the whole file
+    f.read_to_end(&mut buffer)?;
+
+    // find offset (starting address of pixel array) at the end of the file header
+    // last 4 bytes of the header
+    let offset_array: [u8; 4] = buffer[10..14]
+        .try_into()
+        .map_err(|_| Error::other("Could not convert header slice into array"))?;
+    let offset = u32::from_le_bytes(offset_array) as usize;
+
+    println!("The offset is {:#x}", offset);
+
+    // first pixel
+    println!("The first pixel is {:#x?}", &buffer[offset..(offset + 3)]);
+
     Ok(())
 }
