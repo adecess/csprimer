4a111af806830a231fd91d2c7327521449872ada
adecess <axel.saliniere@protonmail.com>
2025-11-04 23:40:42 +0100

    image-rotate: carry out rotation

--- computer-systems/bits-and-bytes/image-rotate/image_rotate_rust/src/main.rs
+++ computer-systems/bits-and-bytes/image-rotate/image_rotate_rust/src/main.rs
@@ -1,9 +1,8 @@
 use std::fs::File;
-use std::io::{Error, Read};
+use std::io::{Error, Read, Write};
 
 fn main() -> std::io::Result<()> {
     let mut f = File::open("teapot.bmp")?;
-
     let mut buffer = Vec::new();
 
     // read the whole file
@@ -33,6 +32,27 @@ fn main() -> std::io::Result<()> {
         "File width is {} and height is {}",
         bitmap_width, bitmap_height
     );
+
+    // create rotated bmp file
+    let mut new_file = File::create("new-rotated.bmp")?;
+
+    let mut rotated: Vec<u8> = vec![];
+
+    // iterate over each row
+    for target_row in 0..bitmap_height as usize {
+        // iterate each pixel
+        for target_col in 0..bitmap_width as usize {
+            let source_row = target_col;
+            let source_col = bitmap_width as usize - 1 - target_row;
+
+            let n = offset + 3 * (source_row * bitmap_width as usize + source_col);
+
+            rotated.extend_from_slice(&buffer[n..(n + 3)]);
+        }
+    }
+
+    new_file.write_all(&[&buffer[..offset], &rotated].concat())?;
+    println!("Created new rotated file");
 
     Ok(())
 }
