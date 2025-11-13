e39e9a624c1da4eb036dd6fc7ce9fb0e44a17165
adecess <axel.saliniere@protonmail.com>
2025-11-13 23:00:15 +0100

    tcp-syn-flood: parse per-packet headers

--- computer-systems/bits-and-bytes/tcp-syn-flood/tcp_syn_flood_rust/src/main.rs
+++ computer-systems/bits-and-bytes/tcp-syn-flood/tcp_syn_flood_rust/src/main.rs
@@ -10,10 +10,30 @@ fn main() -> std::io::Result<()> {
     let magic_number = get_four_bytes(&buffer[0..4])?;
     let major_version = get_two_bytes(&buffer[4..6])?;
     let minor_version = get_two_bytes(&buffer[6..8])?;
-    let _link_layer_header = get_four_bytes(&buffer[20..24])?;
+    let link_layer_header_type = get_four_bytes(&buffer[20..24])?;
 
     assert_eq!(magic_number, 0xa1b2c3d4);
     println!("Version number is {}.{}", major_version, minor_version);
+    assert_eq!(link_layer_header_type, 0);
+
+    let mut packet_data_list = Vec::new();
+    let mut slice = &buffer[24..];
+
+    while !slice.is_empty() {
+        let captured_packet_length = get_four_bytes(&slice[8..12])?;
+        let untruncated_packet_length = get_four_bytes(&slice[12..16])?;
+        assert_eq!(captured_packet_length, untruncated_packet_length);
+
+        let data_start = 16;
+        let data_end = data_start + captured_packet_length;
+
+        let packet_data = slice[data_start..data_end].to_vec();
+        packet_data_list.push(packet_data);
+
+        slice = &slice[data_end..];
+    }
+
+    println!("There are {} packets", packet_data_list.len());
 
     Ok(())
 }
