eb62c35a55d9cb71632f106e5a7097869358b865
adecess <axel.saliniere@protonmail.com>
2025-12-07 14:35:27 +0100

    utr-8-truncate: refactor and make more idiomatic

--- computer-systems/bits-and-bytes/utf-8-truncate/utf_8_truncate_rust/src/main.rs
+++ computer-systems/bits-and-bytes/utf-8-truncate/utf_8_truncate_rust/src/main.rs
@@ -8,62 +8,39 @@ fn main() -> std::io::Result<()> {
     f.read_to_end(&mut buffer)?;
 
     let mut lines: Vec<Vec<u8>> = buffer
-        .split(|b| *b == b'\n')
+        .split(|&b| b == b'\n')
+        .filter(|line| !line.is_empty())
         .map(|bytes| bytes.to_vec())
-        .filter(|line| line.len() > 0)
         .collect();
 
     println!("Lines are {:#?}", lines);
 
     let mut new_file = File::create("new-cases")?;
 
-    for line in lines.iter_mut() {
-        // truncate
-        let mut i = line[0] as usize;
-        i += 1;
-
-        while i < line.len() {
-            if get_byte_position(line[i]) == Utf8BytePattern::ContinuationByte {
-                // Move back one index (but don't go negative)
-                if i > 0 {
-                    i -= 1;
-                }
-            } else {
-                line.truncate(i);
-            }
+    for line in &mut lines {
+        if line.is_empty() {
+            continue;
         }
+
+        let mut i = line[0] as usize + 1;
+
+        // Find the truncation point
+        while i < line.len() && (line[i] & 0xC0 == 0x80) {
+            // Move back one index (but don't go negative)
+            i = i.saturating_sub(1);
+        }
+
+        line.truncate(i);
 
         // remove first byte
-        if line.len() > 0 {
-            line.remove(0);
-        }
-        // add line break
+        line.remove(0);
         line.push(b'\n');
+
+        // add line to file
+        new_file.write_all(&line)?;
     }
 
-    let truncated_lines: Vec<u8> = lines.into_iter().flatten().collect();
-
-    new_file.write_all(&truncated_lines)?;
     println!("Created new rotated file");
 
     Ok(())
-}
-
-#[derive(PartialEq)]
-enum Utf8BytePattern {
-    ContinuationByte,
-    FirstByteOfOne,
-    FirstByteOfTwo,
-    FirstByteOfThree,
-    FirstByteOfFour,
-}
-
-fn get_byte_position(b: u8) -> Utf8BytePattern {
-    match b {
-        b if b & 0x80 == 0 => Utf8BytePattern::FirstByteOfOne,
-        b if b & 0xE0 == 0xC0 => Utf8BytePattern::FirstByteOfTwo,
-        b if b & 0xF0 == 0xE0 => Utf8BytePattern::FirstByteOfThree,
-        b if b & 0xF8 == 0xF0 => Utf8BytePattern::FirstByteOfFour,
-        _ => Utf8BytePattern::ContinuationByte,
-    }
 }
