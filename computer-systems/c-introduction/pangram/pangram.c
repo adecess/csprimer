126831cc58bbf3bfa760dcf8785ca757acfb4b7a
adecess <axel.saliniere@protonmail.com>
2025-12-21 19:48:03 +0100

    pangram: solve problem

---
+++ computer-systems/c-introduction/pangram/pangram.c
@@ -0,0 +1,44 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool ispangram(char *s) {
+    size_t len = strlen(s);
+    char unique_letters[len] = {};
+
+    while (*s != '\n') {
+        if (strchr(unique_letters, tolower(*s)) == NULL) {
+            // Safe to append since character is not present in buffer
+            size_t pos = strlen(unique_letters);
+            if (isalpha(*s)) {
+                unique_letters[pos] = tolower(*s);
+                unique_letters[pos + 1] = '\0';
+            }
+
+            s++; // go to next character
+        } else {
+            s++;
+            continue;
+        }
+    }
+
+  return strlen(unique_letters) == 26;
+}
+
+int main() {
+  size_t len;
+  ssize_t read;
+  char *line = NULL;
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    if (ispangram(line))
+      printf("%s", line);
+  }
+
+  if (ferror(stdin))
+    fprintf(stderr, "Error reading from stdin");
+
+  free(line);
+  fprintf(stderr, "ok\n");
+}
