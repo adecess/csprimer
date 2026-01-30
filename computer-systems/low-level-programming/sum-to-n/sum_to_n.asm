d3650645220b935df1c358198f43c013b10c0a58
adecess <axel.saliniere@protonmail.com>
2026-01-30 23:57:46 +0100

    sum-to-n: remove redundant jmp and cmp

--- computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
+++ computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
@@ -3,11 +3,9 @@ global sum_to_n
 
 sum_to_n:
     mov eax, 0
-    jmp add_and_decrement
 
 add_and_decrement:
     add eax, edi
     sub edi, 1
-    cmp edi, 0
-    jg add_and_decrement
+    jge add_and_decrement
     ret
