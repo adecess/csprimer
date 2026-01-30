567fd2bbf03cb5cccbef864ad5e40dda2350f922
adecess <axel.saliniere@protonmail.com>
2026-01-30 23:35:46 +0100

    sum-to-n: refactor to use 1 fewer jump

--- computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
+++ computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
@@ -4,16 +4,13 @@ global sum_to_n
 sum_to_n:
     mov eax, 0
     mov ebx, 0
-    jmp compare_increment
 
 compare_increment:
     cmp ebx, edi
-    jl add_one
+    jl add_one_and_increment
     ret
 
-add_one:
+add_one_and_increment:
     add ebx, 1
-
-add_increment:
     add eax, ebx
     jmp compare_increment
