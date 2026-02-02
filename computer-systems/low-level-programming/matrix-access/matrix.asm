99aa7db868b0023900f5765a0cee79c0b4a9e0a9
adecess <axel.saliniere@protonmail.com>
2026-02-02 23:23:20 +0100

    matrix-acess: one fewer instruction

--- computer-systems/low-level-programming/matrix-access/matrix.asm
+++ computer-systems/low-level-programming/matrix-access/matrix.asm
@@ -6,8 +6,7 @@ matrix:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
-	mov rax, rdx
-	imul rax, rcx ; Compute cols * rindex
-    lea rax, [rdi + rax*4]; Compute xMatrix + L * (cols * rindex)
+	imul rdx, rcx ; Compute cols * rindex
+    lea rax, [rdi + rdx*4]; Compute xMatrix + L * (cols * rindex)
 	mov eax, [rax + r8*4] ; Read from memory[xMatrix + L * (cols * rindex) + L * cindex]
 	ret
