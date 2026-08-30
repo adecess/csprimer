a185ecbf5f0635626f47b3d15a1e67eaaa7b2dd1
adecess <axel.saliniere@protonmail.com>
2026-08-30 18:07:11 +0200

    cputime: calculate elapsed time for every instruction block

--- operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
+++ operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
@@ -1,7 +1,8 @@
+use rustix::thread::sched_getcpu; // rustix provides safe bindings to POSIX APIs
 use std::alloc::{Layout, alloc};
 use std::hint::black_box;
+use std::time::Instant;
 use std::{process, thread, time};
-use rustix::thread::{sched_getcpu}; // rustix provides safe bindings to POSIX APIs
 
 const SLEEP_SEC: time::Duration = time::Duration::from_millis(3000);
 const NUM_MULS: u32 = 100000000;
@@ -9,41 +10,55 @@ const NUM_ALLOCS: u32 = 100000;
 const ALLOC_SIZE: usize = 1000;
 const ALIGN_SIZE: usize = 16; // default malloc align size on x86-64 Unix systems
 
-// TODO define this struct
 #[derive(Default)]
-struct ProfileTimes {}
+struct ProfileTimes {
+    start: Option<Instant>,
+    elapsed: f32,
+}
 
-// TODO populate the given struct with starting information
-fn profile_start(profile_times: &mut ProfileTimes) {}
+fn profile_start(profile_times: &mut ProfileTimes, log: &str) {
+    println!("[pid {}, cpu {}] {}", process::id(), sched_getcpu(), log);
 
-// TODO given starting information, compute and log differences to now
+    profile_times.start = Some(Instant::now());
+}
+
 fn profile_log(profile_times: &mut ProfileTimes) {
-    println!("[pid {}, cpu {}] hey", process::id(), sched_getcpu())
+    profile_times.elapsed = profile_times.start.unwrap().elapsed().as_secs_f32();
+
+    println!(
+        "[pid {}, cpu {}] real {:?}",
+        process::id(),
+        sched_getcpu(),
+        profile_times.elapsed
+    )
 }
 
 fn main() {
     let mut profile_times = ProfileTimes::default();
 
-    // TODO profile doing a bunch of floating point muls
     let mut _x = 1.0f32;
-    profile_start(&mut profile_times);
-    for _ in 0..NUM_MULS {
-        _x *= 1.1;
+    let mut i = 0_32;
+    profile_start(&mut profile_times, &format!("{} fmuls", NUM_MULS));
+    while i < NUM_MULS {
+        // avoid Rust-specific iterator overhead
+        _x *= 1.1f32;
+        i += 1;
     }
-    black_box(&_x);
     profile_log(&mut profile_times);
+    black_box(&_x);
 
-    // TODO profile doing a bunch of allocs
-    profile_start(&mut profile_times);
+    profile_start(
+        &mut profile_times,
+        &format!("{} allocs of size {}", NUM_ALLOCS, ALLOC_SIZE),
+    );
     let layout = Layout::from_size_align(ALLOC_SIZE, ALIGN_SIZE).expect("alignemnt error");
     for _ in 0..NUM_ALLOCS {
-        let ptr = unsafe { alloc(layout) };     // intentional leaking like in the exercise template
+        let ptr = unsafe { alloc(layout) }; // intentional leaking like in the exercise template
         black_box(ptr);
     }
     profile_log(&mut profile_times);
 
-    // TODO profile sleeping
-    profile_start(&mut profile_times);
+    profile_start(&mut profile_times, &format!("sleeping for {:?}", SLEEP_SEC));
     thread::sleep(SLEEP_SEC);
     profile_log(&mut profile_times);
 }
