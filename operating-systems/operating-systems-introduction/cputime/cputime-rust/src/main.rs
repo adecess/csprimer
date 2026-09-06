33ffe1121e903fac02fabbd3efd59adb30f3df98
adecess <axel.saliniere@protonmail.com>
2026-09-06 16:01:22 +0200

    cputime: use only safe Rust and replace rustix with nix crate

--- operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
+++ operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
@@ -1,5 +1,4 @@
-use rustix::thread::sched_getcpu; // rustix provides safe bindings to POSIX APIs
-use std::alloc::{Layout, alloc};
+use nix::sched::sched_getcpu; // nix provides safe bindings to libc
 use std::hint::black_box;
 use std::time::Instant;
 use std::{process, thread, time};
@@ -8,16 +7,17 @@ const SLEEP_SEC: time::Duration = time::Duration::from_millis(3000);
 const NUM_MULS: u32 = 100000000;
 const NUM_ALLOCS: u32 = 100000;
 const ALLOC_SIZE: usize = 1000;
-const ALIGN_SIZE: usize = 16; // default malloc align size on x86-64 Unix systems
 
 #[derive(Default)]
 struct ProfileTimes {
     start: Option<Instant>,
     elapsed: f32,
+    user_time: f32,
+    sys_time: f32
 }
 
 fn profile_start(profile_times: &mut ProfileTimes, log: &str) {
-    println!("[pid {}, cpu {}] {}", process::id(), sched_getcpu(), log);
+    println!("[pid {}, cpu {}] {}", process::id(), sched_getcpu().unwrap(), log);
 
     profile_times.start = Some(Instant::now());
 }
@@ -28,7 +28,7 @@ fn profile_log(profile_times: &mut ProfileTimes) {
     println!(
         "[pid {}, cpu {}] real {:?}",
         process::id(),
-        sched_getcpu(),
+        sched_getcpu().unwrap(),
         profile_times.elapsed
     )
 }
@@ -36,24 +36,23 @@ fn profile_log(profile_times: &mut ProfileTimes) {
 fn main() {
     let mut profile_times = ProfileTimes::default();
 
-    let mut _x = 1.0f32;
+    let mut x = 1.0f32;
     let mut i = 0_32;
     profile_start(&mut profile_times, &format!("{} fmuls", NUM_MULS));
     while i < NUM_MULS {
         // avoid Rust-specific iterator overhead
-        _x *= 1.1f32;
+        x *= 1.1f32;
         i += 1;
     }
+    black_box(&x);
     profile_log(&mut profile_times);
-    black_box(&_x);
 
     profile_start(
         &mut profile_times,
         &format!("{} allocs of size {}", NUM_ALLOCS, ALLOC_SIZE),
     );
-    let layout = Layout::from_size_align(ALLOC_SIZE, ALIGN_SIZE).expect("alignemnt error");
     for _ in 0..NUM_ALLOCS {
-        let ptr = unsafe { alloc(layout) }; // intentional leaking like in the exercise template
+        let ptr = vec![0; ALLOC_SIZE]; // simple and safe way to heap allocate a specific size
         black_box(ptr);
     }
     profile_log(&mut profile_times);
