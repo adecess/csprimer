271879634326c19055634ca0ccf6f4399c6eed26
adecess <axel.saliniere@protonmail.com>
2026-09-07 09:11:51 +0200

    cputime: complete exercise with user and sys times

--- operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
+++ operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
@@ -1,9 +1,11 @@
 use nix::sched::sched_getcpu; // nix provides safe bindings to libc
+use nix::sys::resource::{Usage, UsageWho, getrusage};
+use nix::sys::time::TimeVal;
 use std::hint::black_box;
-use std::time::Instant;
-use std::{process, thread, time};
+use std::time::{Duration, Instant};
+use std::{process, thread};
 
-const SLEEP_SEC: time::Duration = time::Duration::from_millis(3000);
+const SLEEP_SEC: Duration = Duration::from_millis(3000);
 const NUM_MULS: u32 = 100000000;
 const NUM_ALLOCS: u32 = 100000;
 const ALLOC_SIZE: usize = 1000;
@@ -12,32 +14,54 @@ const ALLOC_SIZE: usize = 1000;
 struct ProfileTimes {
     start: Option<Instant>,
     elapsed: f32,
-    user_time: f32,
-    sys_time: f32
+    usage_start: Option<Usage>,
+    usage_end: Option<Usage>,
 }
 
 fn profile_start(profile_times: &mut ProfileTimes, log: &str) {
-    println!("[pid {}, cpu {}] {}", process::id(), sched_getcpu().unwrap(), log);
+    println!(
+        "[pid {}, cpu {}] {}",
+        process::id(),
+        sched_getcpu().unwrap(),
+        log
+    );
 
     profile_times.start = Some(Instant::now());
+    profile_times.usage_start = Some(getrusage(UsageWho::RUSAGE_SELF).unwrap());
 }
 
 fn profile_log(profile_times: &mut ProfileTimes) {
     profile_times.elapsed = profile_times.start.unwrap().elapsed().as_secs_f32();
 
+    profile_times.usage_end = Some(getrusage(UsageWho::RUSAGE_SELF).unwrap());
+
+    let user_start = duration_from_timeval(profile_times.usage_end.unwrap().user_time());
+    let user_end = duration_from_timeval(profile_times.usage_start.unwrap().user_time());
+    let user_cpu_time = user_start.saturating_sub(user_end).as_secs_f32();
+
+    let sys_start = duration_from_timeval(profile_times.usage_end.unwrap().system_time());
+    let sys_end = duration_from_timeval(profile_times.usage_start.unwrap().system_time());
+    let sys_cpu_time = sys_start.saturating_sub(sys_end).as_secs_f32();
+
     println!(
-        "[pid {}, cpu {}] real {:?}",
+        "[pid {}, cpu {}] real {:?} user: {:?} sys: {:?}",
         process::id(),
         sched_getcpu().unwrap(),
-        profile_times.elapsed
+        profile_times.elapsed,
+        user_cpu_time,
+        sys_cpu_time
     )
+}
+
+fn duration_from_timeval(tv: TimeVal) -> Duration {
+    Duration::new(tv.tv_sec() as u64, (tv.tv_usec() * 1000) as u32)
 }
 
 fn main() {
     let mut profile_times = ProfileTimes::default();
 
     let mut x = 1.0f32;
-    let mut i = 0_32;
+    let mut i = 0_u32;
     profile_start(&mut profile_times, &format!("{} fmuls", NUM_MULS));
     while i < NUM_MULS {
         // avoid Rust-specific iterator overhead
