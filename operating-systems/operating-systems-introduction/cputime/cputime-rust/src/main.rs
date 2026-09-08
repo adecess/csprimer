588c821c35373207e4de6d7bfdb4e73ca7cecccf
adecess <axel.saliniere@protonmail.com>
2026-09-08 23:15:49 +0200

    cputime: clean solution

--- operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
+++ operating-systems/operating-systems-introduction/cputime/cputime-rust/src/main.rs
@@ -2,6 +2,7 @@ use nix::sched::sched_getcpu; // nix provides safe bindings to libc
 use nix::sys::resource::{Usage, UsageWho, getrusage};
 use nix::sys::time::TimeVal;
 use std::alloc::{Layout, alloc};
+use std::error::Error;
 use std::hint::black_box;
 use std::time::{Duration, Instant};
 use std::{process, thread};
@@ -15,78 +16,87 @@ const ALIGN_SIZE: usize = 16; // 64-bit Linux minimum alignment
 #[derive(Default)]
 struct ProfileTimes {
     start: Option<Instant>,
-    elapsed: f32,
     usage_start: Option<Usage>,
     usage_end: Option<Usage>,
 }
 
-fn profile_start(profile_times: &mut ProfileTimes, log: &str) {
-    println!(
-        "[pid {}, cpu {}] {}",
-        process::id(),
-        sched_getcpu().unwrap(),
-        log
-    );
+fn profile_start(profile_times: &mut ProfileTimes, log: &str) -> Result<(), nix::errno::Errno> {
+    println!("[pid {}, cpu {}] {}", process::id(), sched_getcpu()?, log);
 
     profile_times.start = Some(Instant::now());
-    profile_times.usage_start = Some(getrusage(UsageWho::RUSAGE_SELF).unwrap());
+    profile_times.usage_start = Some(getrusage(UsageWho::RUSAGE_SELF)?);
+
+    Ok(())
 }
 
-fn profile_log(profile_times: &mut ProfileTimes) {
-    profile_times.elapsed = profile_times.start.unwrap().elapsed().as_secs_f32();
+fn profile_log(profile_times: &mut ProfileTimes) -> Result<(), nix::errno::Errno> {
+    profile_times.usage_end = Some(getrusage(UsageWho::RUSAGE_SELF)?);
 
-    profile_times.usage_end = Some(getrusage(UsageWho::RUSAGE_SELF).unwrap());
+    let (Some(start), Some(usage_start), Some(usage_end)) = (
+        profile_times.start.as_ref(),
+        profile_times.usage_start.as_ref(),
+        profile_times.usage_end.as_ref(),
+    ) else {
+        return Err(nix::errno::Errno::EINVAL);
+    };
 
-    let user_start = duration_from_timeval(profile_times.usage_end.unwrap().user_time());
-    let user_end = duration_from_timeval(profile_times.usage_start.unwrap().user_time());
-    let user_cpu_time = user_start.saturating_sub(user_end).as_secs_f32();
+    let elapsed = start.elapsed().as_secs_f32();
 
-    let sys_start = duration_from_timeval(profile_times.usage_end.unwrap().system_time());
-    let sys_end = duration_from_timeval(profile_times.usage_start.unwrap().system_time());
-    let sys_cpu_time = sys_start.saturating_sub(sys_end).as_secs_f32();
+    let user_cpu_time = duration_from_timeval(usage_end.user_time())
+        .saturating_sub(duration_from_timeval(usage_start.user_time()))
+        .as_secs_f32();
+
+    let sys_cpu_time = duration_from_timeval(usage_end.system_time())
+        .saturating_sub(duration_from_timeval(usage_start.system_time()))
+        .as_secs_f32();
 
     println!(
         "[pid {}, cpu {}] real {:?} user: {:?} sys: {:?}",
         process::id(),
-        sched_getcpu().unwrap(),
-        profile_times.elapsed,
+        sched_getcpu()?,
+        elapsed,
         user_cpu_time,
         sys_cpu_time
-    )
+    );
+
+    Ok(())
 }
 
 fn duration_from_timeval(tv: TimeVal) -> Duration {
     Duration::new(tv.tv_sec() as u64, (tv.tv_usec() * 1000) as u32)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut profile_times = ProfileTimes::default();
 
     let mut x = 1.0f32;
     let mut i = 0_u32;
-    profile_start(&mut profile_times, &format!("{} fmuls", NUM_MULS));
+    profile_start(&mut profile_times, &format!("{} fmuls", NUM_MULS))?;
     while i < NUM_MULS {
         // avoid Rust-specific iterator overhead
         x *= 1.1f32;
         i += 1;
     }
     black_box(&x);
-    profile_log(&mut profile_times);
+    profile_log(&mut profile_times)?;
 
     profile_start(
         &mut profile_times,
         &format!("{} allocs of size {}", NUM_ALLOCS, ALLOC_SIZE),
-    );
+    )?;
+
+    let layout = Layout::from_size_align(ALLOC_SIZE, ALIGN_SIZE)?;
     for _ in 0..NUM_ALLOCS {
-        let layout = Layout::from_size_align(ALLOC_SIZE, ALIGN_SIZE).unwrap();
         unsafe {
             let ptr = alloc(layout); // intentional leaking like in the C template
             black_box(ptr);
         }
     }
-    profile_log(&mut profile_times);
+    profile_log(&mut profile_times)?;
 
-    profile_start(&mut profile_times, &format!("sleeping for {:?}", SLEEP_SEC));
+    profile_start(&mut profile_times, &format!("sleeping for {:?}", SLEEP_SEC))?;
     thread::sleep(SLEEP_SEC);
-    profile_log(&mut profile_times);
+    profile_log(&mut profile_times)?;
+
+    Ok(())
 }
